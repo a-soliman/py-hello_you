2d3348086f6d8a0ba40451e7b071157e8f44d8c2
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-23 07:31:24 -0800

    collected the user data in variables

--- hello.py
+++ hello.py
@@ -7,3 +7,9 @@
  5. Create output text.
  6. Print output to screen
 '''
+
+name 	= input('What is your name?: ')
+age 	= int(input('How old are you?: '))
+city 	= input('In which city do you live?: ')
+hobies 	= input('What do you enjoy doing?: ') 
+
